f5dbe0f2f8c092cfe56f156c960fc83b75fc4af4
jdellamag <94094932+jdellamag@users.noreply.github.com>
2022-05-11 00:58:47 -0700

    Create crypto_price_feed.py

---
+++ crypto_price_feed.py
@@ -0,0 +1,33 @@
+# Raw Package
+import numpy as np
+import pandas as pd
+
+#Data Source
+import yfinance as yf
+
+#Data viz
+import plotly.graph_objs as go
+
+# Token Variables (can rename variables but need to keep ticker,period,interval the same)
+data1 = yf.download(tickers='BTC-USD', period = '4h', interval = '1h')
+
+data2 = yf.download(tickers='ETH-USD', period = '4h', interval = '1h')
+
+data3 = yf.download(tickers='MATIC-USD', period = '4h', interval = '1h')
+
+print("BTC-USD")
+print(data1)
+print("ETH-USD")
+print(data2)
+print("MATIC-USD")
+print(data3)
+
+
+
+print
+
+
+
+
+
+# https://medium.com/analytics-vidhya/python-how-to-get-bitcoin-data-in-real-time-less-than-1-second-lag-38772da43740
