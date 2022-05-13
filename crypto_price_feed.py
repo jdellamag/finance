f2135ce23addf5efb3a3f833608ec542be757aab
jdellamag <94094932+jdellamag@users.noreply.github.com>
2022-05-12 21:28:01 -0700

    Update crypto_price_feed.py

--- crypto_price_feed.py
+++ crypto_price_feed.py
@@ -10,10 +10,9 @@ import plotly.graph_objs as go
 
 # Token Variables (can rename variables but need to keep ticker,period,interval)
 data1 = yf.download(tickers='BTC-USD', period = '4h', interval = '1h')
-
 data2 = yf.download(tickers='ETH-USD', period = '4h', interval = '1h')
-
 data3 = yf.download(tickers='MATIC-USD', period = '4h', interval = '1h')
+data4 = yf.download(tickers='LUNA-USD', period = '4h', interval = '1h')
 
 print("BTC-USD")
 print(data1)
@@ -21,7 +20,8 @@ print("ETH-USD")
 print(data2)
 print("MATIC-USD")
 print(data3)
-
+print("LUNA-USD")
+print(data4)
 
 
 
